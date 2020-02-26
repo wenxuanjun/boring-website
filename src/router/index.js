6c4d4d237e2fe4d007202d82d2a40456071f0862
wenxuanjun <wendster@163.com>
2020-02-26 15:25:44 +0800

    Removed NProgress and enabled history mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,44 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import NProgress from 'nprogress'
-import 'nprogress/nprogress.css'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: () => import('../views/Home.vue')
-  },
-  {
-    path: '/malageed',
-    name: 'malageed',
-    component: () => import('../views/Malageed.vue')
-  },
-  {
-    path: '/tools',
-    name: 'tools',
-    component: () => import('../views/Tools.vue')
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import('../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  routes
+export default new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/Home.vue')
+    },
+    {
+      path: '/malageed',
+      name: 'malageed',
+      component: () => import('../views/Malageed.vue')
+    },
+    {
+      path: '/tools',
+      name: 'tools',
+      component: () => import('../views/Tools.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/About.vue')
+    }
+  ]
 })
-
-router.beforeEach((to, from, next) => {
-  NProgress.start()
-  next()
-})
-
-router.afterEach(() => {
-  NProgress.done()
-})
-
-export default router
